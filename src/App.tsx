b7fb0e0485d9fb61adc2f659fdcbcaff9a5d5fa2
Artem Zhyrnyi <ic3ego@gmail.com>
2025-01-11 11:30:07 +0200

    fix the issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import cn from 'classnames';
 import debounce from 'lodash.debounce';
 
 import './App.scss';
 
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-import { Dropdown } from './components/Dropdown';
 
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
   const [appliedQuery, setAppliedQuery] = useState('');
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [isActive, setActive] = useState(false);
 
   const applyQuery = debounce(setAppliedQuery, 500);
 
@@ -40,12 +41,52 @@ export const App: React.FC = () => {
             : 'No selected person'}
         </h1>
 
-        <Dropdown
-          people={filteredPeople}
-          onPersonSelect={handlePersonSelect}
-          onQueryChange={handleQueryChange}
-          query={query}
-        />
+        <div className={cn('dropdown', { 'is-active': isActive })}>
+          <div className="dropdown-trigger">
+            <input
+              type="text"
+              placeholder="Enter a part of the name"
+              className="input"
+              data-cy="search-input"
+              value={query}
+              onChange={handleQueryChange}
+              onFocus={() => setActive(true)}
+              onBlur={() =>
+                setTimeout(() => {
+                  setActive(false);
+                }, 25)
+              }
+            />
+          </div>
+
+          {!filteredPeople.length || (
+            <div
+              className="dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+            >
+              <div className="dropdown-content">
+                {filteredPeople.map(person => (
+                  <div
+                    key={person.slug}
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                  >
+                    <p
+                      className={cn('is-clickable', {
+                        'has-text-link': person.sex === 'm',
+                        'has-text-danger': person.sex === 'f',
+                      })}
+                      onClick={() => handlePersonSelect(person)}
+                    >
+                      {person.name}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
 
         {!filteredPeople.length && (
           <div
